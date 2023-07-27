b992a4c468627dc42d7a2d816622b63987c25180
kukunju <132256077+kukunju@users.noreply.github.com>
2023-07-27 13:39:51 +0000

    routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     post "users/guest_sign_in", to: "user/sessions#guest_sign_in"
+    get '/users', to: 'user/registrations#new'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
